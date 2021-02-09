ece929b7075d1eee7dd5a4932dc0f529850af3c0
hiro <hiro@carmapenguin.net>
2021-02-09 22:07:54 +0900

    :rocket:Only ffmpeg process once.

--- anime_indicator.sh
+++ anime_indicator.sh
@@ -4,18 +4,21 @@ FFMPEG="/usr/bin/ffmpeg"
 PYTHON="/usr/bin/python"
 
 INPUT="$1"
+
+START="00:00:00"
+END="00:24:40"
+
 NAME=`basename "$INPUT" | sed 's/\.[^\.]*$//' `
 CUT=1
-START="hh:mm:ss"
-END="hh:mm:ss"
 
 WKDIR=$HOME/anime_indicator
 IMG_DIR=$WKDIR/img
 IMGSIZE="480x270"
 
 if [ $CUT = 1 ]; then
-	$FFMPEG -y -ss $START -to $END -i "$INPUT" -vcodec copy -an $WKDIR/tmp.mp4
-	INPUT=$WKDIR/tmp.mp4
+	PARM="-ss $START -to $END"
+else
+	PARM=" "
 fi
 
 if [ -d "$IMG_DIR" ]; then
@@ -24,16 +27,15 @@ fi
 mkdir -p "$IMG_DIR"
 
 # frame
-$FFMPEG $OVERWRITE -i "$INPUT" \
+$FFMPEG $OVERWRITE $PARM -i "$INPUT"  \
 		-vf framestep=1 -vsync 0 -q:v 5 -s $IMGSIZE \
 		"$IMG_DIR"/image_%05d.jpg
 
 # frame count
 cd "$IMG_DIR"
 COUNT=`ls -U1 | wc -l`
+echo "frame count: $COUNT"
 
-$PYTHON $WKDIR/hist_feature.py "$IMG_DIR"/ $COUNT > ret_"$NAME".txt
-cat ret_"$NAME".txt
+$PYTHON $WKDIR/hist_feature.py "$IMG_DIR"/ $COUNT | tee $WKDIR/result/ret_"$NAME".txt
 
 exit
-
