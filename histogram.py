8f1af2cd2802210b9709f28d75b1055857cb2eb5
hiro <hiro@carmapenguin.net>
2021-02-17 11:38:01 +0900

    :sparkles:Split the image and compare previous frame.

--- histogram.py
+++ histogram.py
@@ -4,51 +4,59 @@ import numpy as np
 
 DEBUG = 1
 WRITE = 1
+div = 30
 
 def hist(IMG_DIR, files, IMG_OUT_DIR):
 
-    diff = prev = cnt = 0
-    scene_prev = scene_cnt = 0
+    score = total_score = 0
+    cnt = scene_cnt = 0
 
-    target_img = prev_img = np.zeros((270, 480, 3), np.uint8)
-    target_histB = cv2.calcHist([target_img], [0], None, [256], [0, 256])
-    target_histG = cv2.calcHist([target_img], [1], None, [256], [0, 256])
-    target_histR = cv2.calcHist([target_img], [2], None, [256], [0, 256])
+    prev_img_path = IMG_DIR + files[0]
+    prev_img = cv2.imread(prev_img_path)
+    h, w, c = prev_img.shape
+    hClp = h / div
+    wClp = w / div
+
     for file in files:
-        comparing_img_path = IMG_DIR + file
-        comparing_img = cv2.imread(comparing_img_path)
-        comparing_histB = cv2.calcHist([comparing_img], [0], None, [256], [0, 256])
-        comparing_histG = cv2.calcHist([comparing_img], [1], None, [256], [0, 256])
-        comparing_histR = cv2.calcHist([comparing_img], [2], None, [256], [0, 256])
+        comp_img_path = IMG_DIR + file
+        comp_img = cv2.imread(comp_img_path)
+        comp_hists, scores = [[[]]], []
+        for x in range(0,div):
+            for y in range(0,div):
+                comp_Clp = \
+                    comp_img[int(y * hClp):int((y + 1) * hClp), \
+                             int(x * wClp):int((x + 1) * wClp)]
+                prev_Clp = \
+                    prev_img[int(y * hClp):int((y + 1) * hClp), \
+                             int(x * wClp):int((x + 1) * wClp)]
+                color = ('b','g','r')
+                for i,col in enumerate(color):
+                    comp_hist = cv2.calcHist([comp_Clp],[i],None,[256],[0,256])
+                    prev_hist = cv2.calcHist([prev_Clp],[i],None,[256],[0,256])
+                    score = cv2.compareHist(prev_hist, comp_hist, cv2.HISTCMP_CORREL)
+                    scores.append(score)
+        prev_img = comp_img
+        score = 1 - np.mean(scores) # 0 to 2
+        total_score = total_score + score
 
-        retB = cv2.compareHist(target_histB, comparing_histB, cv2.HISTCMP_CORREL)
-        retG = cv2.compareHist(target_histG, comparing_histG, cv2.HISTCMP_CORREL)
-        retR = cv2.compareHist(target_histR, comparing_histR, cv2.HISTCMP_CORREL)
-        ret = 1 - ((retB + retG + retR) / 3)  # 0 to 2
-
-#        if ret < 1.006:
-#            continue
-        diff = diff + ret
-        if abs(prev - ret) > 0:
+        if score > 0 or cnt == 0:
             if DEBUG == 1:
-                print(file, ret, abs(prev - ret), diff)
+                print(file, score, total_score)
             cnt = cnt + 1
-            prev = ret
-
         else:
             if WRITE == 1:
-                cv2.imwrite(IMG_OUT_DIR + 'eq' + str(cnt).zfill(5) + '.jpg', comparing_img)
-            continue
+                z_cnt = str(cnt).zfill(5)
+                cv2.imwrite(IMG_OUT_DIR + 'eq' + z_cnt + '.jpg', comp_img)
+            if DEBUG == 1:
+                print('equal:', file, score, total_score)
         #scene frame
-        if abs(scene_prev - ret) > 0.15:
+        if score > 1:
             if WRITE == 1:
                 z_scene_cnt = str(scene_cnt).zfill(5)
-                cv2.imwrite(IMG_OUT_DIR + 'scene' + z_scene_cnt + '.jpg', prev_img)
-                if DEBUG == 1:
-                    print('scene', file, ret, abs(scene_prev - ret), diff)
+                cv2.imwrite(IMG_OUT_DIR + 'scene' + z_scene_cnt + '.jpg', comp_img)
+            if DEBUG == 1:
+                print('scene:', file, score, total_score)
             scene_cnt = scene_cnt + 1
-            scene_prev = ret
-            prev_img = comparing_img
-            
-    return diff, cnt, scene_cnt
+
+    return total_score, cnt, scene_cnt
 
